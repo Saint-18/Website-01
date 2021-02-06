b886c4f9c02fb9a998ae75f04217464c79e0bd85
Eli <Eli@Eli.com>
2021-02-06 14:48:33 -0600

    Working calc

--- WebApplication1/WebApplication1/Pages/Index.cshtml.cs
+++ WebApplication1/WebApplication1/Pages/Index.cshtml.cs
@@ -15,11 +15,40 @@ namespace WebApplication1.Pages
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
+            
         }
+
+
+        [BindProperty]
+        public int num1 { get; set; }
+
+        [BindProperty]
+        public int num2 { get; set; }
+
+        [BindProperty]
+        public string num3 { get; set; }
+
 
         public void OnGet()
         {
+            
+        }
+
+
+        public void OnPost()
+        {
+            var mynum1 = this.num1;
+            var mynum2 = this.num2;
+
+            var mynum3 = num1 + num2;
+
+            num3 = mynum3.ToString();
 
         }
+
     }
+
+
+
+
 }
